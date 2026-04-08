eb5f3260ae4645fb7b9b0b67c648125d97b4e8de
mage <arbaz.k480@yahoo.com>
2026-04-08 17:13:41 +0500

    implement the homepage UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,85 @@
+import ListHeading from "@/components/ListHeading";
+import SubscriptionCard from "@/components/SubscriptionCard";
+import UpcomingSubscriptionCard from "@/components/UpcomingSubscriptionCard";
+import { HOME_BALANCE, HOME_SUBSCRIPTIONS, HOME_USER, UPCOMING_SUBSCRIPTIONS } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import images from "@/constants/images";
 import "@/global.css";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
-import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
 import { styled } from "nativewind";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import {useState} from "react";
 const SafeAreaView = styled(RNSafeAreaView)
 
+const HeaderLayout = () => (
+    <>
+        <View className="home-header">
+            <View className="home-user">
+                <Image source={images.avatar} className="home-avatar" />
+                <Text className="home-user-name">{HOME_USER?.name ?? "John Doe"}</Text>
+            </View>
+            <Pressable className="home-add-icon-container" onPress={() => console.log('add button pressed')}>
+                <Image source={icons.add} className="home-add-icon" />
+            </Pressable>
+        </View>
+
+        <View className="home-balance-card">
+            <Text className="home-balance-label">Balance</Text>
+
+            <View className="home-balance-row">
+                <Text className="home-balance-amount">{formatCurrency(HOME_BALANCE.amount)}</Text>
+                <Text className="home-balance-date">{dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}</Text>
+            </View>
+        </View>
+
+        <View>
+            <ListHeading title="Upcoming" />
+
+            <FlatList data={UPCOMING_SUBSCRIPTIONS}
+
+                      renderItem={({ item }) => (
+                          <UpcomingSubscriptionCard {...item} />
+                      )}
+
+                      keyExtractor={(item) => item.id.toString()}
+                      horizontal={true}
+                      showsHorizontalScrollIndicator={false}
+                      ListEmptyComponent={<Text>No upcoming renewals yet.</Text>}
+            />
+        </View>
+
+        <ListHeading title="All Subscriptions" />
+    </>
+)
+
 export default function App() {
+    const [expandedSubscriptionID, setExpandedSubscriptionID] = useState<string | null>(null)
     return (
         <SafeAreaView className="flex-1 bg-background p-5">
-            <Text className="text-5xl font-sans-extrabold ">
-                Home
-            </Text>
-            <Link href="/settings" className="mt-4 font-sans-bold bg-primary rounded text-white p-4">settings</Link>
-            <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold bg-primary rounded text-white p-4">Sign up</Link>
 
+                <FlatList
+                    ListHeaderComponent={<HeaderLayout />}
+
+                    data={HOME_SUBSCRIPTIONS}
+                    showsVerticalScrollIndicator={false}
+                    keyExtractor={(item) => item.id}
+                    extraData={expandedSubscriptionID}
+                    contentContainerClassName='pb-30'
+                    ItemSeparatorComponent={() => <View className="h-4" />}
+                    renderItem={({ item }) => (
+                        <SubscriptionCard
+                            {...item}
+                            expanded={expandedSubscriptionID === item.id}
+                            onPress={() =>
+                                setExpandedSubscriptionID((currentId) =>
+                                    currentId === item.id ? null : item.id
+                                )
+                            }
+                        />
+                    )}
+                />
         </SafeAreaView>
     );
 }
---
+++ components/SubscriptionCard.tsx
@@ -0,0 +1,92 @@
+import {formatCurrency, formatStatusLabel, formatSubscriptionDateTime} from '@/lib/utils'
+import clsx from 'clsx'
+import {Image, Pressable, Text, TouchableOpacity, View} from 'react-native'
+import React from "react";
+
+const SubscriptionCard = ({ id, icon, startDate, color, expanded, onPress, status, onCancelPress, name, price, currency, billing, category, renewalDate, plan, paymentMethod}:SubscriptionCardProps) => {
+  return (
+        <Pressable
+            onPress={onPress}
+            className={
+                clsx('sub-card',
+                    expanded ? 'sub-card-expanded' : 'bg-card')}
+            style={ !expanded && color ? { backgroundColor: color } : undefined
+            }
+        >
+         <View className='sub-head'>
+             <View className='sub-main'>
+
+                <View className='sub-icon-container'>
+                    <Image source={icon} className='sub-icon' />
+                </View>
+                <View className='sub-copy'>
+                    <Text numberOfLines={1} className='sub-title'>{name}</Text>
+                    <Text numberOfLines={1} ellipsizeMode='tail' className='sub-meta'>
+                        {category?.trim() || plan?.trim() || (renewalDate ?
+                            formatSubscriptionDateTime(renewalDate) : '')}
+                    </Text>
+                </View>
+             </View>
+
+             <View className='sub-price-box'>
+                 <Text className='sub-price'>{formatCurrency(price, currency)}</Text>
+                 <Text className='sub-billing'>{billing}</Text>
+             </View>
+         </View>
+
+        {expanded && (
+            <View className='sub-expanded'>
+                <View className='sub-details'>
+                    <View className='sub-row'>
+                        <View className='sub-row-copy'>
+                            <Text className='sub-label'>Payment:</Text>
+                            <Text className='sub-value' numberOfLines={1} ellipsizeMode='tail'>{paymentMethod}</Text>
+                        </View>
+                        <TouchableOpacity className="list-action">
+                            <Text className="list-action-text">Manage</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View className='sub-row'>
+                        <View className='sub-row-copy'>
+                            <Text className='sub-label'>Category:</Text>
+                            <Text className='sub-value'  numberOfLines={1} ellipsizeMode='tail'>{category?.trim() || plan?.trim() || ''}</Text>
+                        </View>
+                        <TouchableOpacity className="list-action">
+                            <Text className="list-action-text">Change</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View className='sub-row'>
+                        <View className='sub-row-copy'>
+                            <Text className='sub-label'>Started:</Text>
+                            <Text className='sub-value'  numberOfLines={1} ellipsizeMode='tail'>{startDate ? formatSubscriptionDateTime(startDate) : ''}</Text>
+                        </View>
+                        <TouchableOpacity className="list-action">
+                            <Text className="list-action-text">Change</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View className='sub-row'>
+                        <View className='sub-row-copy'>
+                            <Text className='sub-label'>Renewal Date:</Text>
+                            <Text className='sub-value'  numberOfLines={1} ellipsizeMode='tail'>{renewalDate ? formatSubscriptionDateTime(renewalDate) : ''}</Text>
+                        </View>
+                        <TouchableOpacity className="list-action">
+                            <Text className="list-action-text">Change</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View className='sub-row'>
+                        <View className='sub-row-copy'>
+                            <Text className='sub-label'>Status:</Text>
+                            <Text className='sub-value'  numberOfLines={1} ellipsizeMode='tail'>{status ? formatStatusLabel(status) : ''}</Text>
+                        </View>
+                        <TouchableOpacity className="list-action">
+                            <Text className="list-action-text">Change</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        )}
+        </Pressable>
+)
+}
+
+export default SubscriptionCard
